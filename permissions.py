92e825bde9a80a9b95eeccfc4cf262f9aa82ceb1
05remla <05remla@gmail.com>
2015-10-20 21:57:22 +0000

    Update permissions.py

--- permissions.py
+++ permissions.py
@@ -1,37 +1,41 @@
 #!/usr/bin/python
-from os import path, listdir
-from os import lstat, walk
-from os import system
-import sqlite3, sys
+import os
+import sqlite3
 
+database = '/permissions.py'
 
 def Gather():
+    global databae
+    conn = sqlite3.connect(database)
+    c = conn.cursor()
+
     print('Building database...')
     ExclusionList = ['media', 'dev', 'tmp', 'cdrom', 'rofs', 'mnt',
                      'proc', 'sys']
-    conn = sqlite3.connect('/permissions.db')
-    c = conn.cursor()
+
     c.execute('''CREATE TABLE permissions (bits text, uid text, gid text, item text)''')
     conn.commit()
 
-    for i in listdir('/'):
+    for i in os.listdir('/'):
         if not (i in ExclusionList):       
-            for Root, Dirs, Files in walk('/' + i):
+            for Root, Dirs, Files in os.walk('/' + i):
                 for Dir in Dirs:
-                    CurrentDir = path.join(Root, Dir)
-                    bits = oct(lstat(CurrentDir).st_mode)[4:]
-                    uid = lstat(CurrentDir).st_uid
-                    gid = lstat(CurrentDir).st_gid
+                    CurrentDir = os.path.join(Root, Dir)
+                    stats = os.lstat(CurrentDir)
+                    bits = oct(stats[0])[4:]
+                    uid = stats[4]
+                    gid = stats[5]
                     c.execute("INSERT INTO permissions VALUES (?,?,?,?)", 
                               (bits, uid, gid, CurrentDir)) 
                     conn.commit()
 
                     
                 for File in Files:
-                    CurrentFile = path.join(Root, File)
-                    bits = oct(lstat(CurrentFile).st_mode)[4:]
-                    uid = lstat(CurrentFile).st_uid
-                    gid = lstat(CurrentFile).st_gid
+                    CurrentFile = os.path.join(Root, File)
+                    stats = os.lstat(CurrentFile)
+                    bits = oct(stats[0])[4:]
+                    uid = stats[4]
+                    gid = stats[5]
                     c.execute("INSERT INTO permissions VALUES (?,?,?,?)", 
                               (bits, uid, gid, CurrentFile)) 
                     conn.commit()
@@ -41,24 +45,22 @@ def Gather():
       
 
 def Restore():
-    #x = 0
-    con = sqlite3.connect('/permissions.db')
-    for line in con.iterdump():
-        #x += 1
+    global databae
+    conn = sqlite3.connect(database)
+
+    for line in conn.iterdump():
         try:
             split_string = line.split("'")
-            if (path.exists(split_string[7])):
+            if (os.path.exists(split_string[7])):
                 bits, uid = split_string[1], split_string[3]
                 gid, item = split_string[5], split_string[7]
-                system('chmod ' + bits + ' ' + item)
-                system('chown ' + uid + ' ' + item)
-                system('chgrp ' + gid + ' ' + item)
+                os.system('chmod ' + bits + ' ' + item)
+                os.system('chown ' + uid + ' ' + item)
+                os.system('chgrp ' + gid + ' ' + item)
                 print('processed ' + item)
         except:
-            pass
-        
-        #if (x >= 4):
-            #sys.exit()
+            pass        
+    #conn.close()
 
             
 if __name__ == '__main__':
